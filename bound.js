238bb7226bf6a36f6c2d254223cb39b3c721df7f
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-05-21 14:04:48 -0500

    Add promiseErrorFirst

--- bound.js
+++ bound.js
@@ -1,7 +1,12 @@
 Object.assign(exports, {
   promise(event) {
     return new Promise(res => {
-      this.once(event, res);
+      this.once(event, (...data) => res(data));
+    });
+  },
+  promiseErrorFirst(event) {
+    return new Promise((res, rej) => {
+      this.once(event, (err, ...data) => err ? rej(err), res(data));
     });
   },
   async *asyncIterator(event) {
