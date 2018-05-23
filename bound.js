c042de3e7faee8590e50259b1bc9f8860fd162f7
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-05-22 20:56:49 -0500

    Make asyncIterator better

--- bound.js
+++ bound.js
@@ -9,9 +9,36 @@ Object.assign(exports, {
       this.once(event, (err, ...data) => err ? rej(err) : res(data));
     });
   },
-  async *asyncIterator(event) {
-    while (true) {
-      yield await promise.call(this, event);
+  asyncIterator(...args) {
+    return {
+      [Symbol.iterator]: this[Symbol.iterator].bind(this, ...args)
+    };
+  },
+  defaultEvent: "message",
+  [Symbol.iterator](event = this.defaultEvent, count) {
+    let res, rej, reps = 0;
+    const events = [];
+    this.on(event, (...data) => {
+      events.push(data)
+      update();
+    })
+    function update() {
+      if (!events.length) return;
+      res(events.shift());
     }
+    return {
+      next: () => {
+        if (count != null) reps++;
+        let ret = {
+          value: new Promise((resolve, reject) => {
+            res = resolve;
+            rej = reject;
+          }),
+          done: count === reps - 1
+        }
+        update();
+        return ret;
+      }
+    };
   }
 });
