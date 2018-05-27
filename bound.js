a9cfdf96809a90179a2b485930651ca4d43dbadc
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-05-27 17:30:46 -0500

    Add options objects, improve asyncIterator
    `asyncIterator` has `count`, `doneEvent`, and `doneTest`, and it can now monitor other events
    to see if it's done, `promise` has an `errorFirst` option that replaces the `promiseErrorFirst` method.

--- bound.js
+++ bound.js
@@ -1,27 +1,57 @@
 Object.assign(exports, {
-  promise(event) {
-    return new Promise(res => {
-      this.once(event, (...data) => res(data));
-    });
-  },
-  promiseErrorFirst(event) {
-    return new Promise((res, rej) => {
-      this.once(event, (err, ...data) => err ? rej(err) : res(data));
-    });
+  defaultEvent: "message",
+  defaultDoneEvent: "done",
+  defaultErrorEvent: "error",
+  then(cb) {
+    return this.promise().then(cb);
   },
   asyncIterator(...args) {
     return {
       [Symbol.iterator]: exports[Symbol.iterator].bind(this, ...args)
     };
   },
-  defaultEvent: "message",
-  [Symbol.iterator](event = this.defaultEvent, count) {
-    let res, rej, reps = 0;
+  promise(event = this.defaultEvent, { errorFirst }) {
+    return new Promise((res, rej) => {
+      this.once(event, (...data) => {
+        if (errorFirst) {
+          const [err, ...rest] = data;
+          if (err) rej(err);
+          else res(rest);
+        } else res(data);
+      });
+    });
+  },
+  [Symbol.iterator](
+    event = this.defaultEvent,
+    { count, doneEvent = this.defaultDoneEvent, doneTest }
+  ) {
+    let res, rej;
+    let reps = 0;
     const events = [];
     this.on(event, (...data) => {
-      events.push(data)
+      events.push(data);
       update();
-    })
+    });
+    let done = false;
+    if (
+      typeof doneTest !== "undefined" &&
+      (typeof doneTest === "boolean" ? doneTest : true)
+    ) {
+      let doneCb;
+      switch (typeof doneTest) {
+        case "boolean":
+          doneCb = a => a;
+          break;
+        case "function":
+          doneCb = doneTest;
+          break;
+        default:
+          doneCb = a => a === doneTest;
+      }
+      this.on(doneEvent, (...args) => {
+        done = !!doneCb(...args);
+      });
+    }
     function update() {
       if (!events.length) return;
       res(events.shift());
@@ -29,13 +59,14 @@ Object.assign(exports, {
     return {
       next: () => {
         if (count != null) reps++;
+        if (done) return { done: true };
         let ret = {
           value: new Promise((resolve, reject) => {
             res = resolve;
             rej = reject;
           }),
           done: count === reps - 1
-        }
+        };
         update();
         return ret;
       }
